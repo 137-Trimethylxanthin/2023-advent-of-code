557c6a6566fa4ddf1bc8b88d6abfd89b03c10dd4
137-Trimethylxanthin <xxv.many@gmail.com>
2023-12-05 06:50:05 +0100

    quick and dirty 🫠

---
+++ 05/first_half/target/debug/deps/first_half-a5bf1a95bd72206b.d
@@ -0,0 +1,6 @@
+/home/maxi/Documents/code-projects/advent_of_code/05/first_half/target/debug/deps/first_half-a5bf1a95bd72206b.rmeta: src/main.rs src/test.rs
+
+/home/maxi/Documents/code-projects/advent_of_code/05/first_half/target/debug/deps/first_half-a5bf1a95bd72206b.d: src/main.rs src/test.rs
+
+src/main.rs:
+src/test.rs:
---
+++ 05/first_half/target/debug/first_half.d
@@ -0,0 +1,1 @@
+/home/maxi/Documents/code-projects/advent_of_code/05/first_half/target/debug/first_half: /home/maxi/Documents/code-projects/advent_of_code/05/first_half/src/main.rs /home/maxi/Documents/code-projects/advent_of_code/05/first_half/src/test.rs
