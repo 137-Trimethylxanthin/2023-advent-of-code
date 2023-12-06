6ebf19c7ebb603347e593fb9c205e450ceffc705
137-Trimethylxanthin <xxv.many@gmail.com>
2023-12-06 07:03:55 +0100

    test

--- 06/first_half/target/debug/deps/first_half-a5bf1a95bd72206b.d
+++
@@ -1,6 +0,0 @@
-/home/maxi/Documents/code-projects/advent_of_code/06/first_half/target/debug/deps/first_half-a5bf1a95bd72206b.rmeta: src/main.rs src/test.rs
-
-/home/maxi/Documents/code-projects/advent_of_code/06/first_half/target/debug/deps/first_half-a5bf1a95bd72206b.d: src/main.rs src/test.rs
-
-src/main.rs:
-src/test.rs:
--- 06/first_half/target/debug/deps/first_half-f77293540ffcae33.d
+++
@@ -1,6 +0,0 @@
-/home/maxi/Documents/code-projects/advent_of_code/06/first_half/target/debug/deps/first_half-f77293540ffcae33: src/main.rs src/test.rs
-
-/home/maxi/Documents/code-projects/advent_of_code/06/first_half/target/debug/deps/first_half-f77293540ffcae33.d: src/main.rs src/test.rs
-
-src/main.rs:
-src/test.rs:
