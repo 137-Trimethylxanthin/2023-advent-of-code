1139c1c73f9d20dab041a0882fbeb0c36be35852
Methyltheobromin <95408324+137-Trimethylxanthin@users.noreply.github.com>
2023-12-06 08:49:30 +0100

    Delete 05/second_half/target directory

--- 05/second_half/target/debug/deps/second_half-dde0ade8e95b9163.d
+++
@@ -1,6 +0,0 @@
-/home/maxi/Documents/code-projects/advent_of_code/05/second_half/target/debug/deps/second_half-dde0ade8e95b9163: src/main.rs src/test.rs
-
-/home/maxi/Documents/code-projects/advent_of_code/05/second_half/target/debug/deps/second_half-dde0ade8e95b9163.d: src/main.rs src/test.rs
-
-src/main.rs:
-src/test.rs:
--- 05/second_half/target/release/deps/second_half-4b9e795cbdd3f7b3.d
+++
@@ -1,6 +0,0 @@
-/home/maxi/Documents/code-projects/advent_of_code/05/second_half/target/release/deps/second_half-4b9e795cbdd3f7b3: src/main.rs src/test.rs
-
-/home/maxi/Documents/code-projects/advent_of_code/05/second_half/target/release/deps/second_half-4b9e795cbdd3f7b3.d: src/main.rs src/test.rs
-
-src/main.rs:
-src/test.rs:
